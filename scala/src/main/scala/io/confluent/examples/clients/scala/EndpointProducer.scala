8df40478b3ec0dcba73ad301f33441600981511b
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-01-16 11:43:02 -0500

    Update EndpointProducer.scala

--- scala/src/main/scala/io/confluent/examples/clients/scala/EndpointProducer.scala
+++ scala/src/main/scala/io/confluent/examples/clients/scala/EndpointProducer.scala
@@ -1,20 +1,6 @@
 /**
-  * Copyright 2020 Confluent Inc.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
+  * Created using example code from Confluent examples here: https://github.com/confluentinc/examples/tree/7.0.1-post/clients/cloud/scala
   */
-
-  //this works 
 package io.confluent.examples.clients.scala
 
 import java.io.FileReader
@@ -39,7 +25,6 @@ object EndpointProducer extends App {
   val topicName = args(1)
   val MAPPER = new ObjectMapper
   val props = buildProperties(configFileName)
-  //createTopic(topicName, props)
   val producer = new KafkaProducer[String, String](props)
 
   val callback = new Callback {
@@ -53,7 +38,7 @@ object EndpointProducer extends App {
 
 
 
-  val fileName = "/home/ec2-user/endpoint.csv"
+  val fileName = <path to endpoint mock data> 
   
 
     for (line <- Source.fromFile(fileName).getLines().drop(1)) { // Dropping the column names
