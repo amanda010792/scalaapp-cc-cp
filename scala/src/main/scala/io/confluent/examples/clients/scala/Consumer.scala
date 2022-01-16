46259c47eaf61ec746e2d282e5d05c26efbda7d9
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2022-01-16 13:06:37 -0500

    Update Consumer.scala

--- scala/src/main/scala/io/confluent/examples/clients/scala/Consumer.scala
+++ scala/src/main/scala/io/confluent/examples/clients/scala/Consumer.scala
@@ -1,17 +1,5 @@
 /**
-  * Copyright 2020 Confluent Inc.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
+  * Adapted from: https://github.com/confluentinc/examples/tree/7.0.1-post/clients/cloud/scala
   */
 package io.confluent.examples.clients.scala
 
@@ -48,7 +36,7 @@ import scala.collection.JavaConversions._
       val properties = new Properties()
       properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
       properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-      properties.put(ConsumerConfig.GROUP_ID_CONFIG, "scala_example_group_3")
+      properties.put(ConsumerConfig.GROUP_ID_CONFIG, "scala_example_group")
       properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
       properties.load(new FileReader(configFileName))
       properties
